3e5cf62c9cb0e92f9e930d5ac9c3af8d2b542770
Dev 400 <umairasif384@gmail.com>
2026-09-11 17:41:03 +0500

    fevicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import ChatWidget from "@/components/chat/ChatWidget";
 
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -12,10 +13,15 @@ const inter = Inter({
   display: "swap",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "JKV Global",
   description:
     "JKV Global provides access to global financial markets through professional trading platforms and account services.",
+
+  icons: {
+    icon: "/JKVFevicon.ico",
+    shortcut: "/JKVFevicon.ico",
+  },
 };
 
 export default function RootLayout({
